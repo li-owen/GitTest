078549e3e80a790ef2e93ad2c46b9afb167fc565
bobochanya <2103821184@qq.com>
2024-02-04 19:55:11 +0800

    first commit

---
+++ src/main/java/com/bobochanya/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.bobochanya.git;
+
+/**
+ * @description: <pre>Git Test</pre> 
+ * @author bobochanya
+ * @date 2024/2/4 19:40
+ * @version 0.0.1
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Hello Git !");
+	}
+}
